6eaa02682299f5447d72a3977b3999aa3191317e
Alex Guo <aguo2@uiowa.edu>
2025-04-09 15:40:30 -0500

    set up frontend structure

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/getImage/<id>")
+def get_image(id):
+    pass
+
+@app.route('upload')
+def upload_receipt():
+    pass
+
